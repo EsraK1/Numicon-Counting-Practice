c4acbc69602918d5f80c0339cc4ecc78ba11d365
esra.kaya <esra.kaya@ovo.com>
2024-04-12 22:27:23 +0100

    initial commit and goal for the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const num1 = Math.floor(Math.random() * 10);
+  const num2 = Math.floor(Math.random() * 10);
+  const num3 = num1 + num2;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //I want to create an app that will allow teachers to be able to do addition practice using numicons.
+    //Numicons will be chosen at random and the students will have to add them together.
+    //I want to hide the answer so that teachers can give students time to think about the answer.
+    //In the future I would like to allow teachers to choose the resource that they would like to do addition practice with.
+
+    <>
+      <h1>Addition practice App</h1>
+      <div>
+        <button>{num1}</button>
+        <button>{num2}</button>
+        <button>{num3}</button>
+      </div>
+    </>
   );
 }
 
